260cdd20d012632f1418dfbc2ad7cec3187bcfb5
OmarElFakih <omarelfakih97@gmail.com>
2019-08-26 08:46:35 -0400

    music controller and audio mixer created

---
+++ EC-5/Assets/OmarAssets/_scripts/MusicController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MusicController : MonoBehaviour
+{
+    public static MusicController instance = null;
+
+    public AudioMixerSnapshot menu;
+    public AudioMixerSnapshot gameplay;
+    public float transitionSeconds;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        /*Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);*/
+        menu.TransitionTo(transitionSeconds);
+
+        Debug.Log("scene loaded");
+    }
+
+    public void ToGameplay()
+    {
+        gameplay.TransitionTo(transitionSeconds);
+    }
+
+}
---
+++ EC-5/Assets/OmarAssets/_scripts/SceneLoaderTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderTest : MonoBehaviour
+{
+
+    public void loadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+
+    public void transition()
+    {
+        MusicController.instance.ToGameplay();
+    }
+
+}
