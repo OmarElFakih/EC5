116b7c49c47b80af01459acb101af06cc9c8efe9
Jose Luis Moncada <joseluismoncadaostos@gmail.com>
2019-08-26 00:25:21 -0400

    Jose Assets, Description below
    Random Spawns
    Souls Particle System
    Fonts
    Parallax Effects
    Boat Movement

---
+++ EC-5/Assets/JoseAssets/Script/Spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Enemy;
+    float random_px;
+    Vector3 SpawnerPosition;
+    public float SpawnRate = 2f;
+    float NextSpawn = 0f;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > NextSpawn)
+        {
+            NextSpawn = Time.time + SpawnRate;
+            random_px = Random.Range(-17, 17);
+            SpawnerPosition = new Vector3(random_px, transform.position.y,transform.position.z);
+            Instantiate(Enemy, SpawnerPosition, Quaternion.identity);
+        }
+    }
+}
