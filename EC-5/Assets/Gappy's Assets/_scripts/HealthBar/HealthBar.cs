c710b974f779982eb37473da19808cb86e0d1870
Daniel Rivero <danielrivero2000@gmail.com>
2019-08-26 19:19:23 -0400

    health and other things

---
+++ EC-5/Assets/Gappy's Assets/_scripts/HealthBar/HealthBar.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    [Header("UI elements")]
+    public Image image;
+
+    [Header("Data")]
+    public float health;
+    public Gradient gradient;
+    private float currentHealth, ratio;
+
+    private void UpdateUI()
+    {
+        ratio = currentHealth / health;
+        image.fillAmount = ratio;
+        image.color = gradient.Evaluate(ratio);
+    }
+
+    private void InitializeData()
+    {
+        currentHealth = health;
+        UpdateUI();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        //play effects
+        //play other effects
+    }
+
+    // Start is called before the first frame update
+    void Start() => InitializeData();
+
+    // Update is called once per frame
+    void Update() => UpdateUI();
+}
---
+++ EC-5/Assets/Gappy's Assets/_scripts/HealthBar/VitalPart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VitalPart : MonoBehaviour
+{
+    private HealthBar hb;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Projectile"))
+        {
+            hb.TakeDamage(0.05f);
+            Destroy(other);
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (gameObject.CompareTag("Spirit"))
+        {
+            if (other.CompareTag("Enemy")) hb.TakeDamage(other.GetComponent<Soul>().data.damage);
+        }
+    }
+
+    void Start()
+    {
+        hb = FindObjectOfType<HealthBar>();
+    }
+}
