b4c3ba671334ca05ccca56bfba061475baf58950
OmarElFakih <omarelfakih97@gmail.com>
2019-08-27 00:18:45 -0400

    aaaaaaaaaaaaa

--- EC-5/Assets/JoseAssets/Script/Spawner.cs
+++ EC-5/Assets/JoseAssets/Script/Spawner.cs
@@ -7,20 +7,31 @@ public class Spawner : MonoBehaviour
     public GameObject Enemy;
     float random_px;
     Vector3 SpawnerPosition;
-    public float SpawnRate = 2f;
-    float NextSpawn = 0f;
+   // public float SpawnRate = 2f;
+   // float NextSpawn = 0f;
 
    
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.time > NextSpawn)
+       /* if(Time.time > NextSpawn)
         {
             NextSpawn = Time.time + SpawnRate;
             random_px = Random.Range(-17, 17);
             SpawnerPosition = new Vector3(random_px, transform.position.y,transform.position.z);
             Instantiate(Enemy, SpawnerPosition, Quaternion.identity);
+        }*/
+    }
+
+    public void Spawn()
+    {
+        if (Random.value > .5f)
+        {
+            random_px = Random.Range(-17, 17);
+            SpawnerPosition = new Vector3(random_px, transform.position.y, transform.position.z);
+            Instantiate(Enemy, SpawnerPosition, Quaternion.identity);
         }
     }
+
 }
---
+++ EC-5/Assets/OmarAssets/_scripts/Timer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    public UnityEvent OnTimer;
+
+    [SerializeField]
+    private bool _loop = false;
+
+    [SerializeField]
+    private bool _executeOnStart = false;
+
+    [SerializeField]
+    private float _intervalLength = 0f;
+
+    public bool lerpIntervals;
+
+    [SerializeField]
+    private float _targetInterval = 0f;
+
+    [SerializeField]
+    private float _lerpT = 0;
+
+    private float _nextTimer = 0f;
+
+    public bool canExecute = true;
+
+
+    private void Awake()
+    {
+        if (!_executeOnStart)
+        {
+            _nextTimer = Time.time + _intervalLength;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > _nextTimer && canExecute)
+        {
+            OnTimer.Invoke();
+
+            if (lerpIntervals)
+            {
+                _intervalLength = Mathf.Lerp(_intervalLength, _targetInterval, _lerpT * Time.deltaTime);
+            }
+
+            if (_loop)
+            {
+                _nextTimer = Time.time + _intervalLength;
+            }
+        }
+    }
+
+
+    public float PercentageLeft()
+    {
+        return (_nextTimer - Time.time) / _targetInterval;
+    }
+
+}
