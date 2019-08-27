424ace65f0f53c7ac48602148c19dbba367eba51
OmarElFakih <omarelfakih97@gmail.com>
2019-08-27 02:02:15 -0400

    spawners adisted

---
+++ EC-5/Assets/OmarAssets/_scripts/OSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OSpawner : MonoBehaviour
+{
+    public GameObject mob;
+
+    public void Spawn()
+    {
+        if (!HealthBar.gameIsOver && Random.value > .5)
+        {
+            Instantiate(mob, transform.position, Quaternion.identity);
+        }
+    }
+}
