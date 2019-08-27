47ca966b870c8216b6d699154a070949510220f5
Jose Luis Moncada <joseluismoncadaostos@gmail.com>
2019-08-27 00:30:17 -0400

    Update Spawner

--- EC-5/Assets/JoseAssets/Script/Spawner.cs
+++ EC-5/Assets/JoseAssets/Script/Spawner.cs
@@ -6,21 +6,28 @@ public class Spawner : MonoBehaviour
 {
     public GameObject Enemy;
     float random_px;
+    public float delaytime;
     Vector3 SpawnerPosition;
     public float SpawnRate = 2f;
     float NextSpawn = 0f;
 
-   
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        if(Time.time > NextSpawn)
-        {
-            NextSpawn = Time.time + SpawnRate;
-            random_px = Random.Range(-17, 17);
-            SpawnerPosition = new Vector3(random_px, transform.position.y,transform.position.z);
-            Instantiate(Enemy, SpawnerPosition, Quaternion.identity);
-        }
+        delaytime = Random.Range(0, 60);
+        delay(delaytime);
+    }
+
+    public void delay(float delaytime)
+    {
+
+        InvokeRepeating("spawnSomething", 5, delaytime);
+    }
+    // Update is called once per frame
+   
+    public void spawnSomething()
+    {
+        random_px = Random.Range(-17, 17);
+        SpawnerPosition = new Vector3(random_px, transform.position.y, transform.position.z);
+        Instantiate(Enemy, SpawnerPosition, Quaternion.identity);
     }
 }
