a6c38ec487be83f653300d4884a3196244277b79
Jose Luis Moncada <joseluismoncadaostos@gmail.com>
2019-08-26 21:28:01 -0400

    pause

---
+++ EC-5/Assets/JoseAssets/Script/Pause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+   
+    public bool paused;
+
+  public void stopGame()
+    {
+        if (!paused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+           
+            Time.timeScale = 1;
+        }
+        paused = !paused;
+    }
+
+  
+}
